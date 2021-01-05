345f1309ea4288b1b885d5afade560a74f57f925
antoinecln56 <76042823+antoinecln56@users.noreply.github.com>
2021-01-05 17:09:21 +0100

    Antoine

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ namespace LUDOGAMM
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AjouterAdherent());
         }
     }
 }
